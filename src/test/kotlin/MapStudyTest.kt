f3044fe603497ac318e3dc1790b73456bb3447de
hxeyexn <kck85075@kumoh.ac.kr>
2025-04-15 18:36:05 +0900

    test: Map 테스트 - entries

--- src/test/kotlin/MapStudyTest.kt
+++ src/test/kotlin/MapStudyTest.kt
@@ -108,4 +108,22 @@ class MapStudyTest {
         val actualMaxOf: Int = map.maxOf { it.value }
         assertThat(actualMaxOf).isEqualTo(4)
     }
+
+    @Test
+    fun `entries`() {
+        val map =
+            mutableMapOf(
+                "a" to 1,
+                "b" to 2,
+                "c" to 3,
+                "d" to 4,
+            )
+
+        // value 기준으로 최댓값을 구한 후 요소 전체(key, value)를 반환
+        val actual: Map.Entry<String, Int> = map.entries.first { it.value == map.values.max() }
+        assertAll(
+            { assertThat(actual.key).isEqualTo("d") },
+            { assertThat(actual.value).isEqualTo(4) },
+        )
+    }
 }
